fddc74078c065ebc06348396e2d49f7dfe8988d6
LingDXJ <124037687+lingDXJ@users.noreply.github.com>
2023-02-02 16:05:31 +0800

    Create 用户输入.py

---
+++ 用户输入.py
@@ -0,0 +1,11 @@
+message=input()
+print(message)
+
+name = input("Please enter your name: ")
+print(f"\nHello, {name}!")
+
+age = input("How old are you? ")
+age = int(age)
+print(age)
+
+print(4 % 3)
