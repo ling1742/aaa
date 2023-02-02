5ac94697548b48049ab96f7d0d6885eb81197daf
LingDXJ <124037687+lingDXJ@users.noreply.github.com>
2023-02-02 16:04:50 +0800

    Create 字典练习.py

---
+++ 字典练习.py
@@ -0,0 +1,6 @@
+"1.使用一个字典来存储一个熟人的信息，包括名、姓、年龄和居"\
+"住的城市。该字典应包含键first_name 、last_name 、age 和city 。将"\
+"存储在该字典中的每项信息都打印出来。"
+
+people={"first_name":122,"last_name":133,"age":18,"city":1876}
+print(people)
